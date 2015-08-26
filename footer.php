3c234f96b343be884f8c368b481c5be669dea58d
unwrittendevin <unwrittendevin@gmail.com>
2015-08-26 09:16:00 -0400

    adding Piwik code
    adding Piwik code on 8/26/15 for inclusion on SU site usage statistics

--- footer.php
+++ footer.php
@@ -51,6 +51,23 @@
 <img src="http://ib.adnxs.com/seg?add=1637448&t=2" width="1" height="1" />
 
 <!-- End of Segment Pixel -->
-
+<!-- Piwik -->
+<script type="text/javascript">
+ var _paq = _paq || [];
+ _paq.push(["setDocumentTitle", document.domain + "/" + document.title]);
+ _paq.push(["setCookieDomain", "*.syr.edu"]);
+ _paq.push(["setDomains", ["*.syr.edu"]]);
+ _paq.push(['trackPageView']);
+ _paq.push(['enableLinkTracking']);
+ (function() {
+   var u="//its-suwi.syr.edu/";
+   _paq.push(['setTrackerUrl', u+'piwik.php']);
+   _paq.push(['setSiteId', 1]);
+   var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+   g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+ })();
+</script>
+<noscript><p><img src="//its-suwi.syr.edu/piwik.php?idsite=1&rec=1" style="border:0;" alt="" /></p></noscript>
+<!-- End Piwik Code -->
 
 </body></html>
